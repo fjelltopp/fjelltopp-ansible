5e54b234e1af603689d7d708ddd5852f8c279b71
Chas Nelson <github@chasnelson.co.uk>
2026-07-10 20:37:04 +0100

    fix: export AWS_DEFAULT_REGION in storage_restore.sh

--- roles/ckan/templates/ckan/storage_restore.sh
+++ roles/ckan/templates/ckan/storage_restore.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 export AWS_REGION="{{ aws_region }}"
+export AWS_DEFAULT_REGION="{{ aws_region }}"
 export AWS_ACCESS_KEY_ID="{{ ckan_backup_access_key }}"
 export AWS_SECRET_ACCESS_KEY="{{ ckan_backup_access_secret }}"
 
